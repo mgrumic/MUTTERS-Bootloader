5a863ce92f12d7d25d322bc50c3b0112fb68e120
grumic <marko.grumic@rt-rk.com>
2018-12-04 08:15:19 +0100

    Added BSP and mutters_vec.c with simple Makefile build

---
+++ tms570LC4357ZWT/mutters/src/mutters_vec.c
@@ -0,0 +1,28 @@
+/*
+ * mutters_vec.c
+ *
+ *  Created on: 4 Dec 2018
+ *      Author: mgrumic
+ */
+
+void _undef (void) {
+	while(1);
+}
+
+void _svc (void) {
+	while(1);
+}
+
+void _prefetch (void) {
+	while(1);
+}
+
+void _dabort (void) {
+	while(1);
+}
+
+void _exit (int x) {
+	(void) x;
+	while(1);
+}
+
